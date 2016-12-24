92661927403269c9433803e6ef7c68cb35df11b8
Alexei Frolov <alexeifrolov3@gmail.com>
2016-12-23 23:14:42 -0500

    Functions to display new views

--- tracker/views.py
+++ tracker/views.py
@@ -20,23 +20,73 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
-from tracker.models import RSAccount
+from tracker.models import RSAccount, Skill
+from tracker.modules import accounttracker
+from tracker.modules import osrsapi
 
 # Tracker main page.
 def index(request):
-    template = loader.get_template('tracker/index.html')
-    return HttpResponse(template.render(request))
+    return render(request, 'tracker/index.html')
 
 # Tracker for a single account.
-def account(request, user):
+def player(request, user, period='week'):
     try:
-        username = RSAccount.objects.get(username__iexact=user).username
+        acc = RSAccount.objects.get(username__iexact=user)
     except RSAccount.DoesNotExist:
-        username = user
+        return render(request, 'tracker/nottracked.html', {'username': user})
 
-    template = loader.get_template('tracker/account.html')
+    datapoints = accounttracker.get_data_range(acc, period)
+    skills = Skill.objects.order_by('skill_id')
+    table_data = []
+
+    # Set up an array of tuples to populate the player's skill table.
+    # The first entry in each tuple is the difference in experience, the second
+    # is difference in rank.
+    for i in range(24):
+        exp = datapoints[0][1][i].experience
+        rank = datapoints[0][1][i].rank
+        sn = skills[i].skillname
+
+        de = exp - datapoints[-1][1][i].experience
+        # Reversed as higher ranks are better.
+        dr = datapoints[-1][1][i].rank - rank
+
+        if de > 0:
+            s0 = '+{:,}'.format(de)
+        else:
+            s0 = '{:,}'.format(de)
+
+        if dr < 0:
+            s1 = '{:,}'.format(dr)
+        elif dr > 0:
+            s1 = '+{:,}'.format(dr)
+        else:
+            s1 = '0'
+        table_data.append((s0, s1, '{:,}'.format(exp), '{:,}'.format(rank), sn))
+
     context = {
-        'username': username,
+        'username': acc.username,
+        'period': period,
+        'table_data': table_data,
+        'lastupdate': datapoints[0][0].time,
     }
 
-    return HttpResponse(template.render(context, request))
+    return render(request, 'tracker/player.html', context)
+
+
+def updateplayer(request):
+    if (request.method != 'GET'):
+        return HttpResponseBadRequest()
+
+    try:
+        dp = accounttracker.track(request.GET['player'])
+    except accounttracker.RecentUpdateError:
+        return HttpResponse('-1')
+    except osrsapi.PlayerNotFoundError:
+        return HttpResponse('-2')
+    except osrsapi.OsrsRequestError:
+        return HttpResponse('-3')
+    except accounttracker.InvalidUsernameError:
+        return HttpResponse('-4')
+
+    return HttpResponse('OK')
