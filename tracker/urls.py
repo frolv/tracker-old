4043d52502253ce687cdb8ac72de71830c8e4547
Alexei Frolov <alexeifrolov3@gmail.com>
2017-02-04 00:05:26 -0500

    Fix current top full table URL

--- tracker/urls.py
+++ tracker/urls.py
@@ -31,7 +31,7 @@ urlpatterns = [
         r'(?P<period>day|week|month|year)/$', views.currentfull),
     url(r'^current/(?P<skill>(1?[0-9]|2[0-3]|99))/'
         r'(?P<period>day|week|month|year)/(?P<page>\d+)$',
-        views.recordsfull),
+        views.currentfull),
     url(r'^records/(?P<skill>(1?[0-9]|2[0-3]|99|100))/$', views.records),
     url(r'^records/(?P<skill>(1?[0-9]|2[0-3]|99|100))/'
         r'(?P<period>day|week|month|year|fivemin)/$', views.recordsfull),
