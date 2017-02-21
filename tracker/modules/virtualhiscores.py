2d5cd2b1eeeefd030ffb63421c5db2b33c601212
Alexei Frolov <alexeifrolov3@gmail.com>
2017-02-20 23:40:05 -0500

    Begin virtual hiscores module

---
+++ tracker/modules/virtualhiscores.py
@@ -0,0 +1,31 @@
+#
+# tracker/modules/virtualhiscores.py
+# Copyright (C) 2016-2017 Alexei Frolov
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+vs_type_names = {
+    'exp': 'Total Experience',
+}
+
+def type_name(vs_type):
+    """
+    """
+
+    global vs_type_names
+    try:
+        return vs_type_names[vs_type]
+    except KeyError:
+        return ''
